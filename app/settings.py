90bed6e7df58e9995e4c5aebed105be9eec7c4f6
marcosdrey <programmarcos05@gmail.com>
2025-01-06 12:38:12 -0300

    Add integrations with AI and notifications via webhooks

--- app/settings.py
+++ app/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +52,7 @@ INSTALLED_APPS = [
     'products',
     'inflows',
     'outflows',
+    'ai',
 ]
 
 LOGIN_URL = 'login'
@@ -153,3 +157,6 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
+
+GEMINI_MODEL = os.getenv('GEMINI_MODEL')
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
--- outflows/signals.py
+++ outflows/signals.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from services.notify import NotifyService
 from .models import Outflow
 
 
@@ -9,3 +11,22 @@ def update_product_quantity(sender, instance, created, **kwargs):
         product = instance.product
         product.quantity -= instance.quantity
         product.save()
+
+
+@receiver(post_save, sender=Outflow)
+def send_events(sender, instance, created, **kwargs):
+    try:
+        if created:
+            notify = NotifyService()
+            data = {
+                'event_type': 'create_outflow',
+                'timestamp': datetime.now().strftime('%Y-%m-%d, %H:%M:%S'),
+                'product': instance.product.title,
+                'product_selling_price': float(instance.product.selling_price),
+                'product_cost_price': float(instance.product.cost_price),
+                'quantity': instance.quantity,
+                'description': instance.description,
+            }
+            notify.send_order_event(data)
+    except:
+        pass
